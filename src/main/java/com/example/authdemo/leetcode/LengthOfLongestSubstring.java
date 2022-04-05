9c404e329329364b3ad2ebb91ceb275bf0b7d68c
szj <shenzhengjiea@gmail.com>
2022-04-05 14:54:37 +0800

    feat: add leetcode

---
+++ src/main/java/com/example/authdemo/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,29 @@
+package com.example.authdemo.leetcode;
+
+/**
+ * 3. 无重复字符的最长子串
+ * {@see https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/}
+ */
+class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        int[] marks = new int[200];
+        int end = 0;
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < chars.length; i++) {
+            marks[chars[i]]++;
+            count++;
+            while (marks[chars[i]] > 1) {
+                marks[chars[end++]]--;
+                count--;
+            }
+            if (count > max) max = count;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("aab"));
+    }
+}
---
+++ src/main/java/com/example/authdemo/leetcode/MergeKLists.java
@@ -0,0 +1,100 @@
+package com.example.authdemo.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 23. 合并K个升序链表
+ * {@see https://leetcode-cn.com/problems/merge-k-sorted-lists/}
+ */
+public class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        ListNode[] list = lists;
+        final ArrayList<ListNode> result = new ArrayList<>();
+        do {
+            result.clear();
+            for (int i = 0; i < list.length; i = i + 2) {
+                if (i + 1 >= list.length) {
+                    result.add(list[i]);
+                } else {
+                    result.add(mergeList(list[i], list[i + 1]));
+                }
+            }
+            list = result.toArray(new ListNode[0]);
+            System.out.println(Arrays.toString(list));
+        } while (result.size() > 1);
+
+        return result.get(0);
+    }
+
+    public ListNode mergeList(ListNode listNode1, ListNode listNode2) {
+        final ListNode listNode = new ListNode();
+        ListNode currentNode = listNode;
+        while (listNode1 != null || listNode2 != null) {
+            if (listNode1 == null) {
+                currentNode.next = new ListNode(listNode2.val);
+                currentNode = currentNode.next;
+                listNode2 = listNode2.next;
+            } else if (listNode2 == null) {
+                currentNode.next = new ListNode(listNode1.val);
+                currentNode = currentNode.next;
+                listNode1 = listNode1.next;
+            } else {
+                if (listNode1.val < listNode2.val) {
+                    currentNode.next = new ListNode(listNode1.val);
+                    currentNode = currentNode.next;
+                    listNode1 = listNode1.next;
+                } else {
+                    currentNode.next = new ListNode(listNode2.val);
+                    currentNode = currentNode.next;
+                    listNode2 = listNode2.next;
+                }
+            }
+        }
+        return listNode.next;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new MergeKLists().mergeKLists(genListNode(new Integer[][]{{1, 4, 5}, {1, 3, 4}, {2, 6}})));
+        System.out.println(new MergeKLists().mergeKLists(genListNode(new Integer[][]{})));
+    }
+
+    static ListNode[] genListNode(Integer[][] list) {
+        ListNode[] listNode = new ListNode[list.length];
+        for (int i = 0; i < list.length; i++) {
+            ListNode currentNode = null;
+            for (int j = 0; j < list[i].length; j++) {
+                if (currentNode == null) {
+                    listNode[i] = new ListNode(list[i][j]);
+                    currentNode = listNode[i];
+                } else {
+                    currentNode.next = new ListNode(list[i][j]);
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return listNode;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
