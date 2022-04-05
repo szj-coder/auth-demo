7811a1be9ed155cab3a0cc5c30a9f58a29493f9c
shen <863606741@qq.com>
2022-04-05 14:54:37 +0800

    feat: add leetcode

---
+++ src/main/java/com/example/authdemo/leetcode/FindMedianSortedArrays.java
@@ -0,0 +1,29 @@
+package com.example.authdemo.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 4. 寻找两个正序数组的中位数
+ * {@see https://leetcode-cn.com/problems/median-of-two-sorted-arrays/}
+ */
+public class FindMedianSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums = new int[2000];
+        int i, j, k;
+        i = j = k = 0;
+        while (i < nums1.length || j < nums2.length) {
+            if (j >= nums2.length || (i < nums1.length && nums1[i] < nums2[j])) {
+                nums[k++] = nums1[i++];
+            } else {
+                nums[k++] = nums2[j++];
+            }
+        }
+        return k % 2 == 0 ? (nums[k / 2] + nums[k / 2 - 1]) * 1.0 / 2 : nums[k / 2];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindMedianSortedArrays().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(new FindMedianSortedArrays().findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+    }
+}
---
+++ src/main/java/com/example/authdemo/leetcode/IsMatch.java
@@ -0,0 +1,43 @@
+package com.example.authdemo.leetcode;
+
+import ch.qos.logback.core.pattern.FormatInfo;
+
+import java.security.PrivilegedAction;
+
+/**
+ * 10. 正则表达式匹配
+ * {@see https://leetcode-cn.com/problems/regular-expression-matching/}
+ */
+public class IsMatch {
+
+    public boolean isMatch(String s, String p) {
+        boolean[][] bytes = new boolean[s.length() + 1][p.length() + 1];
+        bytes[0][0] = true;
+        char[] sChars = s.toCharArray();
+        char[] pChars = p.toCharArray();
+        for (int j = 1; j < p.length() + 1; j++) {
+            for (int i = 0; i < s.length() + 1; i++) {
+                if (pChars[j - 1] == '*') {
+                    bytes[i][j] = bytes[i][j - 2] || (match(i == 0 ? ' ' : sChars[i - 1], pChars[j - 2])
+                            && (i != 0 && (bytes[i - 1][j - 2] || bytes[i - 1][j])));
+                } else {
+                    bytes[i][j] = match(i == 0 ? ' ' : sChars[i - 1], pChars[j - 1])
+                            && (i != 0 && bytes[i - 1][j - 1]);
+                }
+            }
+        }
+        return bytes[s.length()][p.length()];
+    }
+
+    public boolean match(char s, char p) {
+        return p == '.' || s == p;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IsMatch().isMatch("ababbb", "a.*b"));
+        System.out.println(new IsMatch().isMatch("aa", "a"));
+        System.out.println(new IsMatch().isMatch("aa", "a*"));
+        System.out.println(new IsMatch().isMatch("aab", "c*a*b"));
+        System.out.println(new IsMatch().isMatch("ab", ".*"));
+    }
+}
