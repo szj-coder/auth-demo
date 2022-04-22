c58e3a868b89186853ce3edbf1d71be5bfd2d7d4
shen <863606741@qq.com>
2022-04-22 23:09:09 +0800

    feat: leetcode 42

--- src/main/java/com/example/authdemo/leetcode/Calculate.java
+++ src/main/java/com/example/authdemo/leetcode/Calculate.java
@@ -6,7 +6,7 @@ import java.util.Stack;
  * 224. 基本计算器
  * {@see https://leetcode-cn.com/problems/basic-calculator/}
  */
-public class Solution {
+public class Calculate {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
@@ -45,7 +45,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().calculate("1 + 1"));
-        System.out.println(new Solution().calculate(" 2-1 + 2 "));
+        System.out.println(new Calculate().calculate("1 + 1"));
+        System.out.println(new Calculate().calculate(" 2-1 + 2 "));
     }
 }
---
+++ src/main/java/com/example/authdemo/leetcode/Trap.java
@@ -0,0 +1,44 @@
+package com.example.authdemo.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 42. 接雨水
+ * {@see https://leetcode-cn.com/problems/trapping-rain-water/}
+ */
+public class Trap {
+
+    public int trap(int[] height) {
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            leftMax[i] = max;
+            if (height[i] > max)
+                max = height[i];
+        }
+        max = 0;
+        for (int i = height.length - 1; i >= 0; i--) {
+            rightMax[i] = max;
+            if (height[i] > max)
+                max = height[i];
+        }
+
+        max = 0;
+        for (int i = 1; i < height.length - 1; i++) {
+            if (leftMax[i] < height[i] || rightMax[i] < height[i]) {
+                continue;
+            }
+            max += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        System.out.println(Arrays.toString(height));
+        System.out.println(Arrays.toString(leftMax));
+        System.out.println(Arrays.toString(rightMax));
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Trap().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+}
