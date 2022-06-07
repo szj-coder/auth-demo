de39cb2c103cecdb5ee7bb6d75329b8e87e027dd
shen <863606741@qq.com>
2022-06-07 22:46:02 +0800

    add leetcode

---
+++ src/main/java/com/example/authdemo/leetcode/MaxSlidingWindow.java
@@ -0,0 +1,41 @@
+package com.example.authdemo.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 239. 滑动窗口最大值
+ * {@see https://leetcode-cn.com/problems/sliding-window-maximum/}
+ */
+public class MaxSlidingWindow {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        final PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> -a[0]));
+        int i = 0;
+        while (i < k - 1) {
+            priorityQueue.add(new int[]{nums[i], i});
+            i++;
+        }
+        final int[] ret = new int[nums.length - k + 1];
+        while (i < nums.length) {
+            priorityQueue.add(new int[]{nums[i], i});
+            i++;
+//            System.out.println(String.format("%s %s %s", i, k, Arrays.toString(priorityQueue.peek())));
+            while (priorityQueue.peek()[1] < i - k) {
+                priorityQueue.poll();
+//                System.out.println(String.format("%s %s %s", i, k, Arrays.toString(priorityQueue.peek())));
+            }
+//            System.out.println();
+            ret[i - k] = priorityQueue.peek()[0];
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new MaxSlidingWindow().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+        System.out.println(Arrays.toString(new MaxSlidingWindow().maxSlidingWindow(new int[]{1}, 1)));
+        System.out.println(Arrays.toString(new MaxSlidingWindow().maxSlidingWindow(new int[]{1, -1}, 1)));
+        System.out.println(Arrays.toString(new MaxSlidingWindow().maxSlidingWindow(new int[]{-7,-8,7,5,7,1,6,0}, 4)));
+    }
+}
---
+++ src/main/java/com/example/authdemo/leetcode/RemoveNthFromEnd.java
@@ -0,0 +1,36 @@
+package com.example.authdemo.leetcode;
+
+/**
+ * 19. 删除链表的倒数第 N 个结点
+ * {@see https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/}
+ */
+public class RemoveNthFromEnd {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int i,j=i=0;
+        final ListNode head1 = new ListNode();
+        head1.next = head;
+        ListNode headi,headj = headi = head1;
+        while (i++ < n) {
+            headi = headi.next;
+        }
+        while (headi.next != null) {
+            headi = headi.next;
+            headj = headj.next;
+        }
+        headj.next = headj.next.next;
+        return head1.next;
+    }
+
+    public static void main(String[] args) {
+        new RemoveNthFromEnd().removeNthFromEnd(new ListNode(1), 1);
+    }
+}
