cf5daa648ef4ffc24d4e917b3f9c1b3b1c685f25
szj <shenzhengjiea@gmail.com>
2022-11-20 18:10:47 +0800

    add 26,80

---
+++ src/main/java/com/example/authdemo/learn/leetcode/L26RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package com.example.authdemo.learn.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 26. 删除有序数组中的重复项
+ * {@see https://leetcode.cn/problems/remove-duplicates-from-sorted-array/}
+ */
+public class L26RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                continue;
+            }
+            nums[++i] = nums[j];
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+}
---
+++ src/main/java/com/example/authdemo/learn/leetcode/L80RemoveDuplicates2.java
@@ -0,0 +1,35 @@
+package com.example.authdemo.learn.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 80. 删除有序数组中的重复项 II
+ * {@see https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/}
+ */
+public class L80RemoveDuplicates2 {
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int i = 0;
+        int n = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                n++;
+                nums[i + 1] = nums[j];
+                continue;
+            }
+            i += n == 0 ? 1 : 2;
+            n = 0;
+            nums[i] = nums[j];
+        }
+        return i + 1 + (n == 0 ? 0 : 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+}
