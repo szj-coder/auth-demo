937fba8fbacad993c0078c87045277c918f5d6c8
szj <shenzhengjiea@gmail.com>
2022-12-12 10:14:45 +0800

    chore: add leetCode

---
+++ src/main/java/com/example/authdemo/learn/leetcode/L09CQueue.java
@@ -0,0 +1,39 @@
+package com.example.authdemo.learn.leetcode;
+
+import java.util.Stack;
+
+public class L09CQueue {
+
+    public static void main(String[] args) {
+        CQueue cQueue = new CQueue();
+        cQueue.appendTail(3);
+        System.out.println(cQueue.deleteHead());
+        System.out.println(cQueue.deleteHead());
+    }
+
+    static class CQueue {
+
+        private Stack<Integer> mStack = new Stack<>();
+        private Stack<Integer> cStack = new Stack<>();
+
+        public CQueue() {
+        }
+
+        public void appendTail(int value) {
+            mStack.add(value);
+        }
+
+        public int deleteHead() {
+            Stack<Integer> stack = mStack;
+            if (stack.isEmpty() && cStack.isEmpty()) {
+                return -1;
+            }
+            if (cStack.isEmpty()) {
+                while(!stack.isEmpty()) {
+                    cStack.add(stack.pop());
+                }
+            }
+            return cStack.pop();
+        }
+    }
+}
---
+++ src/main/java/com/example/authdemo/learn/leetcode/L155MinStack.java
@@ -0,0 +1,44 @@
+package com.example.authdemo.learn.leetcode;
+
+import java.util.Stack;
+
+public class L155MinStack {
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.min());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.min());
+    }
+
+    static class MinStack {
+
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> minStack = new Stack<>();
+
+        public MinStack() {
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            minStack.push((Math.min(minStack.peek(), x)));
+        }
+
+        public void pop() {
+            minStack.pop();
+            stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int min() {
+            return minStack.peek();
+        }
+    }
+}
