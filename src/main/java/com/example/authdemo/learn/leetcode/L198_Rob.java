5864f10a25bde385013886d08b9b04e3eade62e5
szj <shenzhengjiea@gmail.com>
2024-06-04 18:02:56 +0800

    Create L198_Rob.java

---
+++ src/main/java/com/example/authdemo/learn/leetcode/L198_Rob.java
@@ -0,0 +1,27 @@
+package com.example.authdemo.learn.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 198. 打家劫舍
+ * {@see https://leetcode.cn/problems/house-robber/description/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china}
+ */
+public class L198_Rob {
+    public static int rob(int[] nums) {
+        if (nums.length ==1) {
+            return nums[0];
+        }
+        int[] a = new int[nums.length];
+        a[0] = nums[0];
+        a[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            a[i] = Math.max(a[i - 1], a[i - 2] + nums[i]);
+        }
+        return a[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1, 2, 3, 1}));
+    }
+
+}
